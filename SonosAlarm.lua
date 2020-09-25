e90e6b5e247c1fcefc8bc5b4709d7980265e4b01
Joep Verhaeg <info@joepverhaeg.nl>
2020-09-25 15:46:52 +0200

    Refactor to full XML parsing
    Thanks to @10der on the Fibaro forums.

--- SonosAlarm.lua
+++ SonosAlarm.lua
@@ -1,84 +1,160 @@
---[[
-%% properties
-%% events
-%% globals
---]]
-
-function getAlarms(xml)
-  local b = 0
-  local e = 0
-  local x = ''
-  local alarmId = ''
-  local alarmTime = ''
-  local alarmRecurrence = ''
-  local alarmEnabled = ''
-
-  while b ~= nill do 
-    -- find the first <Alarm ID... /> tag char number...
-    b = string.find(xml, "&lt;Alarm ID", e)
-    if b ~= nill then
-      -- find the closing tag tag char number...
-      e = string.find(xml, "/&gt;", b)
-      -- substring the whole <Alarm .. /> tag...
-      x = string.sub(xml, b, e+3)
-      -- replace the html quote chars with real quotes for string.match
-      x = x:gsub("&quot;", "\"")
-      alarmId         = string.match(x, [[Alarm%sID="([^"]+)]])
-      alarmTime       = string.match(x, [[StartTime="([^"]+)]])
-      alarmRecurrence = string.match(x, [[Recurrence="([^"]+)]])
-      alarmEnabled    = string.match(x, [[Enabled="([^"]+)]])
-      --fibaro:debug(alarmId)
-      --fibaro:debug(alarmTime)
-      --fibaro:debug(alarmRecurrence)
-      --fibaro:debug(alarmEnabled)
-      --fibaro:debug('----------')
-      -- get the current day of the week
-      local dayofweek = os.date("*t").wday-1
-      -- remap ONCE/WEEKDAYS to day numbers for check later on
-      if alarmRecurrence == 'ONCE' then
-        alarmRecurrence = 'ONCE_' .. dayofweek
-      elseif alarmRecurrence == 'WEEKDAYS' then
-        alarmRecurrence = 'WEEKDAYS_12345'
-      end
-      -- check if the alarm is enabled
-      if tonumber(alarmEnabled) == 1 and alarmRecurrence:match(dayofweek) then
-        fibaro:debug('alarm enabled at: ' .. alarmTime)
-      end
+function parseXml(xml)
+    local function dec2char(code)
+        code = tonumber(code)
+        return string.char(code > 255 and 0 or code)
     end
-  end
+
+    local function hex2char(code)
+        code = tonumber(code, 16)
+        return string.char(code > 255 and 0 or code)
+    end
+
+    unescapeCache = {}
+    local function xmlUnescape(s)
+        local r = unescapeCache[s]
+        if not r then
+            local g = string.gsub
+            r = g(s, "&quot;", '"')
+            r = g(r, "&apos;", "'")
+            r = g(r, "&lt;", "<")
+            r = g(r, "&gt;", ">")
+            r = g(r, "&#(%d%d?%d?%d?);", dec2char)
+            r = g(r, "&#x(%x%x?%x?%x?);", hex2char)
+            r = g(r, "&amp;", "&")
+            unescapeCache[s] = r
+        end
+        return r
+    end
+
+    escapeCache = {}
+    local function xmlEscape(s)
+        local r = escapeCache[s]
+        if not r then
+            local g = string.gsub
+            r = g(s, "&", "&amp;")
+            r = g(r, '"', "&quot;")
+            r = g(r, "'", "&apos;")
+            r = g(r, "<", "&lt;")
+            r = g(r, ">", "&gt;")
+            escapeCache[s] = r
+        end
+        return r
+    end
+
+    local namePattern = "[%a_:][%w%.%-_:]*"
+
+    xml = string.gsub(xml, "<!%[CDATA%[(.-)%]%]>", xmlEscape) -- replace CDATA with escaped text
+    xml = string.gsub(xml, "<%?.-%?>", "") -- remove processing instructions
+    xml = string.gsub(xml, "<!%-%-.-%-%->", "") -- remove comments
+    xml = string.gsub(xml, "<!.->", "")
+
+    local root = {}
+    local parents = {}
+    local element = root
+    for closing, name, attributes, empty, text in string.gmatch(
+        xml,
+        "<(/?)(" .. namePattern .. ")(.-)(/?)>%s*([^<]*)%s*"
+    ) do
+        if closing == "/" then
+            local parent = parents[element]
+            if parent and name == element.name then
+                element = parent
+            end
+        else
+            local child = {name = name, attribute = {}}
+            table.insert(element, child)
+            parents[child] = element
+            if empty ~= "/" then
+                element = child
+            end
+            for name, value in string.gmatch(attributes, "(" .. namePattern .. ')%s*=%s*"(.-)"') do
+                child.attribute[name] = xmlUnescape(value)
+            end
+        end
+        if text ~= "" then
+            local child = {text = xmlUnescape(text)}
+            table.insert(element, child)
+            parents[child] = element
+        end
+    end
+    return root[1]
 end
 
-local body   = '<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:ListAlarms xmlns:u=\"urn:schemas-upnp-org:service:AlarmClock:1\"><InstanceID>0</InstanceID></u:ListAlarms></s:Body></s:Envelope>'
-local action = 'urn:schemas-upnp-org:service:AlarmClock:1#ListAlarms'
-local path   = 'AlarmClock/Control'
-local host   = '192.168.2.10:1400'
+function getXmlPath(nodes, ...)
+    nodes = {nodes}
+    local arg = {...}
+    for i, name in ipairs(arg) do
+        local match = {}
+        for i, node in ipairs(nodes) do
+            if node.name == name then
+                match = nodes
+            else
+                for i, child in ipairs(node) do
+                    if child.name == name then
+                        table.insert(match, child)
+                    end
+                end
+            end
+        end
+        nodes = match
+    end
+    return nodes
+end
+ 
+function getAlarms(xml)
+    local dom = parseXml(xml)
+    local results = getXmlPath(dom, "s:Envelope", "s:Body", "u:ListAlarmsResponse", "CurrentAlarmList")
 
---[[
-<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-  <s:Body>
-    <u:ListAlarms xmlns:u="urn:schemas-upnp-org:service:AlarmClock:1">
-    <InstanceID>0</InstanceID>
-    </u:ListAlarms>
-  </s:Body>
-</s:Envelope>
---]]
+    for _, result in ipairs(results) do
+        -- Sonos store alarm list as XML in XML
+        local alarms = getXmlPath(parseXml(result[1].text), "Alarms", "Alarm")
+        for _, alarm in ipairs(alarms) do
+            for k, v in pairs(alarm.attribute) do
+                print(k, v)
+            end
+            print("-------------------------------------")
+        end
+    end
+end
 
-HC2 = net.HTTPClient()
-HC2:request(
-  'http://' .. host .. '/' .. path, {
-    success = function(resp) getAlarms(resp.data) end,
-    error = function(err)
-              fibaro:debug(err.data)
+function sonosRequest()
+    local body =
+        '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:ListAlarms xmlns:u="urn:schemas-upnp-org:service:AlarmClock:1"><InstanceID>0</InstanceID></u:ListAlarms></s:Body></s:Envelope>'
+    local action = "urn:schemas-upnp-org:service:AlarmClock:1#ListAlarms"
+    local path = "AlarmClock/Control"
+    local host = "192.168.2.33:1400"
+
+    --[[
+    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <s:Body>
+        <u:ListAlarms xmlns:u="urn:schemas-upnp-org:service:AlarmClock:1">
+        <InstanceID>0</InstanceID>
+        </u:ListAlarms>
+        </s:Body>
+    </s:Envelope>
+    --]]
+    HC2 = net.HTTPClient()
+    HC2:request(
+        "http://" .. host .. "/" .. path,
+        {
+            success = function(resp)
+                getAlarms(resp.data)                
             end,
-    options = {
-      headers = {
-        ['Content-Type'] = 'text/xml',
-        ['charset'] = 'UTF-8',
-        ['Host'] = host,
-        ['SOAPAction'] = action
-      },
-      data = body,
-      method = 'POST'
-    }
-  }
-)
+            error = function(err)
+                print(err.data)
+            end,
+            options = {
+                headers = {
+                    ["Content-Type"] = "text/xml",
+                    ["charset"] = "UTF-8",
+                    ["Host"] = host,
+                    ["SOAPAction"] = action
+                },
+                data = body,
+                method = "POST"
+            }
+        }
+    )
+end
+
+sonosRequest()
